531787869b7699d9e52188854e482761c5d9e818
Adrian <149798777+adrianlehmann@users.noreply.github.com>
2026-03-07 21:05:00 +0100

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const fs = require("fs");
 const upload = multer({ dest: "uploads/" });
 const EXIF_PATH = "/app/Image-ExifTool-13.52/exiftool";
 const app = express();
-
+/**
 app.post("/exif", upload.single("file"), (req, res) => {
   const filePath = req.file.path;
 
@@ -20,6 +20,42 @@ app.post("/exif", upload.single("file"), (req, res) => {
 
     const result = JSON.parse(stdout);
     res.json(result[0]);
+  });
+});
+*/
+
+app.post("/exif", upload.single("file"), (req, res) => {
+  const filePath = req.file.path;
+
+  const args = ["-overwrite_original"];
+
+  // convert body fields to exiftool args
+  for (const [key, value] of Object.entries(req.body)) {
+    args.push(`-${key}=${value}`);
+  }
+
+  args.push(filePath);
+
+  execFile(EXIF_PATH, args, (err) => {
+
+    if (err) {
+      fs.unlinkSync(filePath);
+      return res.status(500).send(err.message);
+    }
+
+    execFile(EXIF_PATH, ["-json", filePath], (err2, stdout) => {
+
+      fs.unlinkSync(filePath);
+
+      if (err2) {
+        return res.status(500).send(err2.message);
+      }
+
+      const result = JSON.parse(stdout);
+      res.json(result[0]);
+
+    });
+
   });
 });
 
